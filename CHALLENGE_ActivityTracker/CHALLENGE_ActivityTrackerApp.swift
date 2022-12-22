6bdbfbeca1122cc1ef3ad5e46e675c05ff7cec6d
Edmond <mredmondwang@gmail.com>
2022-12-22 10:18:40 +0800

    Initial Commit

---
+++ CHALLENGE_ActivityTracker/CHALLENGE_ActivityTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CHALLENGE_ActivityTrackerApp.swift
+//  CHALLENGE_ActivityTracker
+//
+//  Created by OAA on 22/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct CHALLENGE_ActivityTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
